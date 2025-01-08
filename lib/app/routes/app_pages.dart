8d1e3d8f5bb1eb82aa3b2d48627045a4422361b3
Chhorda <phengchhorda66@gmail.com>
2025-01-08 15:24:14 +0700

    commit merge

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -20,7 +20,7 @@ part 'app_routes.dart';
 class AppPages {
   AppPages._();
 
-  static const INITIAL = Routes.ACCOUNT;
+  static const INITIAL = Routes.TABBAR;
 
   static final routes = [
     GetPage(
